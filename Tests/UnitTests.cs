470c70ec64d3feb96cf108952e26af2c9a9df40b
vrachosdeity <159507242+vrachosdeity@users.noreply.github.com>
2025-10-27 22:20:00 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -3,27 +3,27 @@ namespace GithubActionsLab;
 [TestClass]
 public class Addition
 {
-	[TestMethod]
-	public void Add_Valid_Patino()
-	{
-		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(5, Program.Add("3", "2"));
-		Assert.AreEqual(12, Program.Add("5", "7"));
-	}
+    [TestMethod]
+    public void Add_Valid_Mengelkamp()
+    {
+        Assert.AreEqual(3, Program.Add("1", "2"));
+        Assert.AreEqual(5, Program.Add("3", "2"));
+        Assert.AreEqual(12, Program.Add("5", "7"));
+    }
 
-	[TestMethod]
-	public void Add_Invalid_Patino()
-	{
-		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
-	}
+    [TestMethod]
+    public void Add_Invalid_Mengelkamp()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
+    }
 
-	[TestMethod]
-	public void Add_Null_Patino()
-	{
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
-	}
+    [TestMethod]
+    public void Add_Null_Mengelkamp()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+    }
 }
